5a67fd62c0bf0a7e7c1dba46e490590a807dd663
pawan-kumar766 <pawan1_kumar@fosteringlinux.com>
2023-12-16 02:19:42 +0530

    login page added

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,8 +3,10 @@ import { Button } from '@/components/ui/button';
 import React from 'react';
 import Banner from '../../../public/appBanner.png';
 import Cal from '../../../public/cal.png';
+import CheckIcon from '../../../public/icons/check.svg';
+import Diamond from '../../../public/icons/diamond.svg';
 import Image from 'next/image';
-import { CLIENTS,USERS } from '@/lib/constants';
+import { CLIENTS , USERS , PRICING_CARDS , PRICING_PLANS} from '@/lib/constants';
 import { randomUUID } from 'crypto';
 import { twMerge } from 'tailwind-merge';
 import CustomCard from '@/components/landing-page/custom-card';
@@ -114,7 +116,7 @@ const HomePage = () => {
         >
           {[...Array(2)].map((arr) => (
             <div
-              key={arr}
+              key={randomUUID()}
               className="flex
                 flex-nowrap
                 animate-slide
@@ -122,7 +124,7 @@ const HomePage = () => {
             >
               {CLIENTS.map((client) => (
                 <div
-                  key={client.alt}
+                  key={randomUUID()}
                   className=" relative
                     w-[200px]
                     m-20
@@ -269,6 +271,123 @@ const HomePage = () => {
                 ></CustomCard>
               ))}
             </div>
+          ))}
+        </div>
+      </section>
+      <section className='mt-20 
+      px-4
+      sm:px-6
+
+      '>
+         <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div className='
+        flex
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+
+        '>
+             {PRICING_CARDS.map((card) => (
+            <CustomCard
+              key={randomUUID()}
+              className={clsx(
+                'w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative',
+                {
+                  'border-brand-primaryPurple/70':
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle
+                  className="text-2xl
+                  font-semibold
+              "
+                >
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div
+                        className="hidden dark:block w-full blur-[120px] rounded-full h-32
+                        absolute
+                        bg-brand-primaryPurple/80
+                        -z-10
+                        top-0
+                      "
+                      />
+                      <Image
+                        src={Diamond}
+                        alt="Pro Plan Icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span
+                    className="font-normal 
+                    text-2xl
+                "
+                  >
+                    ${card.price}
+                  </span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ''
+                  )}
+                  <p className="dark:text-washed-purple-800">
+                    {card.description}
+                  </p>
+                  <Button
+                    variant="btn-primary"
+                    className="whitespace-nowrap w-full mt-4"
+                  >
+                    {card.planType === PRICING_PLANS.proplan
+                      ? 'Go Pro'
+                      : 'Get Started'}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul
+                  className="font-normal
+                  flex
+                  mb-2
+                  flex-col
+                  gap-4
+                "
+                >
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map((feature) => (
+                    <li
+                      key={randomUUID()}
+                      className="flex
+                      items-center
+                      gap-2
+                    "
+                    >
+                      <Image
+                        src={CheckIcon}
+                        alt="Check Icon"
+                      />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
           ))}
         </div>
       </section>
---
+++ src/components/landing-page/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const routes =[
+    {title:"Features",href:"#features"},
+    {title:"Features",href:"#features"},
+    {title:"Features",href:"#features"},
+    {title:"Features",href:"#features"},
+    {title:"Features",href:"#features"},
+]
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
